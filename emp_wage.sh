f930652e4d801501306eb834aa4a37a97ae51409
“rbn2000” <anthonyrobin2000@gmail.com>
2023-03-22 00:52:10 +0530

    [Robin] Add: Shell program to assume 20 working days per month

--- emp_wage.sh
+++ emp_wage.sh
@@ -1,19 +1,30 @@
 echo Welcome to Employee Wage Computation Program on Master Branch
 
 
-isPresent=$((RANDOM%3));
 perHourSalary=20;
 workingHour=0;
-case $isPresent in
-        0)
-        echo "Employee is absent";
-        workingHour=0;
-        ;;
+totalSalary=0;
+totalWorkingHour=0;
+day=1;
+while [[ $day -le 20 ]]
+do
+        isPresent=$((RANDOM%3));
+        case $isPresent in
+                0)
+                echo "Employee is absent";
+                workingHour=0;
+                ;;
 
-        1)
-        echo "Employee is working as part time";
-        workingHour=8;
-        ;;
+                1)
+                echo "Employee is present";
+                workingHour=8;
+                ;;
 
-esac
+                2)
+                echo "Employee is working as part time";
+                workingHour=4;
+                ;;
+        esac
+        ((day++));
+done
 
